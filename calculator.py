a032a8342b7b7bdbe2866e405b603cb5d2433c5c
jebarse <jebarse@yandex.ru>
2025-10-06 14:45:14 +0500

    Добавлены операции возведения в степень и квадратный корень

--- calculator.py
+++ calculator.py
@@ -12,4 +12,11 @@ def divide(a. b):
 
 print("simple calc")
 print("avib. +, -, *, /")
-	
+def power(a, b):
+    return a ** b
+def square_root(a):
+    return a ** 0.5
+# Обновить основной код
+print("Улучшенный калькулятор")
+print("Доступные операции: +, -, *, /, ^, sqrt")
+bash
